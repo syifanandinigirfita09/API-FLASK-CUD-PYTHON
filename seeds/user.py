54100459748571f17aec1f4f6d45a12a027820fe
syifanandinigirfita09 <syifanandinigirfita@gmail.com>
2024-01-04 15:09:47 +0700

    first commit

---
+++ seeds/user.py
@@ -0,0 +1,39 @@
+from app.model.user import Users
+import random, string
+from flask_seeder import Seeder, Faker, generator
+
+
+
+class User(Users):
+    def __init__(self, name=None, email=None, password=None):
+        self.name = name 
+        self.email = email
+        self.password = password
+
+    def __str__(self):
+        return "Name=%s, Email=%s, Password=%s" % (self.name, self.email, self.password)
+    
+
+
+class UserSeeder(Seeder):
+
+
+    def run(self):
+
+        name = generator.Name()
+        faker = Faker(
+            cls=User,
+            init={
+                "name": name,
+                "email": ''.join(random.choice(string.ascii_letters) for i in range(10)) + "@mail.com",
+                "password": "secret"
+            }
+        )
+
+
+        for user in faker.create(1):
+            print("Adding user: %s" % user)
+            self.db.session.add(user)
+
+
+    
